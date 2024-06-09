c6777bf934f53901caa012165dcdc7bb6e226cf7
Robin Sunita Roy <58402504+robin113x@users.noreply.github.com>
2024-06-09 18:47:31 +0530

    Create burp.sh

---
+++ burp.sh
@@ -0,0 +1,8 @@
+# Burp Suite Professional 
+
+git clone https://github.com/robin113x/Burp-Suite.git
+
+cd Burp-Suite
+java -jar keygen.jar
+
+"/usr/lib/jvm/java-21-openjdk-amd64/bin/java" "--add-opens=java.desktop/javax.swing=ALL-UNNAMED" "--add-opens=java.base/java.lang=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED" "-javaagent:burploader.jar" "-noverify" "-jar" "/opt/Burp-Suite/burpsuite_pro_v2024.5.1.jar"& > /dev/null
